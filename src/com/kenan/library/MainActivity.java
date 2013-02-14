c3a70937f2cce5bec4e9979b20a63b4c35a6215b
Kenan <kenan.mcgrath@gmail.com>
2013-02-14 23:41:52 +0000

    Added basic actionBar

--- src/com/kenan/library/MainActivity.java
+++ src/com/kenan/library/MainActivity.java
@@ -1,10 +1,6 @@
 package com.kenan.library;
 
 import static com.kenan.library.DownloadClosingTimes.OPENING_TIMES_KEY;
-
-import com.kenan.library.LibraryBookService.Operation;
-
-import android.app.Activity;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -12,30 +8,22 @@ import android.content.IntentFilter;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Menu;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends Activity {
+import com.actionbarsherlock.app.SherlockActivity;
+import com.actionbarsherlock.view.Menu;
+import com.actionbarsherlock.view.MenuItem;
+import com.kenan.library.LibraryBookService.Operation;
+
+//TODO needs to respond to invalid login
+public class MainActivity extends SherlockActivity {
 
 	private static final String TAG = MainActivity.class.toString();
 
-	public static final String ERROR_BROADCAST = "com.kenan.library.MainActivity.error";
-	public static final String CONNECTION_ERROR_BROADCAST = ERROR_BROADCAST
-			+ ".connection";
-	public static final String LOGIN_ERROR_BROADCAST = ERROR_BROADCAST
-			+ ".login";
-	public static final String PARSE_ERROR_BROADCAST = ERROR_BROADCAST
-			+ ".parse";
-
 	TextView openingTimes;
 	ListView list;
-	Button refreshButton;
-	Button renewButton;
 	TextView lastRefresh;
 
 	BookAdapter bookAdapter;
@@ -63,18 +51,6 @@ public class MainActivity extends Activity {
 		}
 	};
 
-	private OnClickListener refreshButtonListener = new OnClickListener() {
-		public void onClick(View v) {
-			updateBookDatabase();
-		}
-	};
-
-	private OnClickListener renewButtonListener = new OnClickListener() {
-		public void onClick(View v) {
-			renewBooks();
-		}
-	};
-
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -88,14 +64,11 @@ public class MainActivity extends Activity {
 		registerReceiver(openingTimesUpdateReceiver, new IntentFilter(
 				DownloadClosingTimes.UPDATE_OPENING_TIMES_INTENT));
 
-		registerReceiver(errorReceiver, new IntentFilter(ERROR_BROADCAST));
+		registerReceiver(errorReceiver, new IntentFilter(
+				LibraryBookService.ERROR_BROADCAST));
 
 		list = (ListView) findViewById(R.id.list);
 		openingTimes = (TextView) findViewById(R.id.closingTimes);
-		refreshButton = (Button) findViewById(R.id.refresh_button);
-		refreshButton.setOnClickListener(refreshButtonListener);
-		renewButton = (Button) findViewById(R.id.renew_button);
-		renewButton.setOnClickListener(renewButtonListener);
 		lastRefresh = (TextView) findViewById(R.id.last_refresh);
 
 		BookDataSource bookDataSource = new BookDataSource(this);
@@ -114,7 +87,7 @@ public class MainActivity extends Activity {
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		getMenuInflater().inflate(R.menu.activity_main, menu);
+		getSupportMenuInflater().inflate(R.menu.activity_main, menu);
 		return true;
 	}
 
@@ -122,13 +95,15 @@ public class MainActivity extends Activity {
 		openingTimes.setText(newOpeningTimes);
 	}
 
-	private void updateBookDatabase() {
+	// Warning: Proguard must exempt this method
+	public void refreshButtonClicked(MenuItem menuItem) {
 		Intent intent = new Intent(this, LibraryBookService.class);
 		intent.putExtra(Operation.KEY, Operation.REFRESH_BOOK_LIST);
 		startService(intent);
 	}
 
-	private void renewBooks() {
+	// Warning: Proguard must exempt this method
+	public void renewAllClicked(MenuItem menuItem) {
 		Intent intent = new Intent(this, LibraryBookService.class);
 		intent.putExtra(Operation.KEY, Operation.RENEW_BOOKS);
 		startService(intent);
